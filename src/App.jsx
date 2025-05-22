40a39bc898b2219f46deed81d083b691b89b0deb
rich-dev <ricardomtzh.hdz@gmail.com>
2025-05-21 23:59:56 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Login from './components/login/login'
+
+
+function App() {
+
+  return (
+    <>
+ <div className="w-full">
+      <Login />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/App_original.jsx
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+   <h1 class="text-3xl font-bold underline">
+    Hello world React Vite Tailwind CSS!
+  </h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
