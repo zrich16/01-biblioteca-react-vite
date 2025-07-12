6265ac5568db048169d16fe4b433d65165dc6a1e
rich-dev <ricardomtzh.hdz@gmail.com>
2025-07-12 11:32:06 -0600

    actualizacion

--- src/pages/libros/detalle.jsx
+++ src/pages/libros/detalle.jsx
@@ -13,7 +13,7 @@ function Detalle() {
       <main style={styles.mainContent}>
                 <form id='formBuscar'>
         <div className="w-full">
-          <div className="grid grid-cols-1 gap-1 items-center max-w-3xl mx-auto">
+          <div className="grid grid-cols-5 gap-1 items-center max-w-3xl mx-auto">
             <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
 
               <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/it.jpg" alt="" />
