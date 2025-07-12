36d1dad16ba05713850afaf9cfb69a63a4c876ae
rich-dev <ricardomtzh.hdz@gmail.com>
2025-07-12 13:56:09 -0600

    3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
