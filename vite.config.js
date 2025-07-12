507c10a98f23094ad76084cb6fa296282e3b9c73
rich-dev <ricardomtzh.hdz@gmail.com>
2025-07-12 12:30:46 -0600

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   base: '/proyecto/',  // Cambia 'mi-app' por el nombre del subdirectorio
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // asegúrate que esté así para desplegar en el root
 });
